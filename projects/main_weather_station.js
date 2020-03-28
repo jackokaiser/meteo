fcf730ac7ae551f0dd252a556eed91aaacd2b443
Jacques KAISER <jkaiser@fzi.de>
2020-03-28 15:24:51 +0100

    More memory efficient code

--- projects/main_weather_station.js
+++ projects/main_weather_station.js
@@ -1,24 +1,23 @@
 function main() {
   clearInterval();
   clearWatch();
-  var readWriteDelay = 1000 * 1;
-  var saveDelay = 1000 * 30;
-  var csvDelay = 1000 * 60 * 60 * 24;
-  var ccsMode = readWriteDelay < 10000 ? 1 : readWriteDelay < 60000 ? 2 : 3;
-
-  var co2Warn = 2000;
-  var humWarn = 55;
+  var rw_d = 1;
+  var s_d = 30;
+  var csv_d = 60 * 60 * 24;
 
   function start(){
     screen.clear();
-    screen.drawString("Screen initialized");
     screen.flip();
   }
 
   I2C1.setup({scl:B6,sda:B7});
   var screen = require("SSD1306").connect(I2C1, start);
   var ScreenManager = require("ScreenManager");
-  var gas = require("CCS811").connectI2C(I2C1, {int: B4, mode: ccsMode, nWake: B5});
+  var gas = require("CCS811").connectI2C(I2C1, {
+    int: B4,
+    mode: rw_d < 10 ? 1 : rw_d < 60 ? 2 : 3,
+    nWake: B5
+  });
   var dht = require("DHT22").connect(A1);
   var fs = require("fs");
 
@@ -26,24 +25,23 @@ function main() {
   var meteo = new Float32Array(5);
 
   var csv;
-  var createNewCsv = function() {
-    var columns = ['chip_temperature', 'temperature', 'humidity', 'co2', 'voc'];
+  var newCsv = function() {
+    var columns = ['chip_temp', 'temp', 'humidity', 'eCO2', 'VOC'];
     var now = new Date();
-    csv = now + "_" + saveDelay + ".csv";
+    csv = now + "_" + s_d + ".csv";
     console.log("New csv file: " + csv  + " for date " + now.toString());
     fs.writeFileSync(csv, columns.join(','));
   };
 
-  createNewCsv();
-  var interCsv = setInterval(createNewCsv, csvDelay);
+  newCsv();
 
   var drawScreen = function() {
     sm.clear();
-    sm.info("temp ", meteo[1].toFixed(1), ' C');
+    sm.info("temp ", meteo[1].toFixed(1), ' °');
     sm.info("hum ", meteo[2].toFixed(0), " %");
     sm.info("eCO2 ", meteo[3], " ppm");
     sm.info("VOC ", meteo[4], " ppm");
-    if ((meteo[3] > co2Warn) || (meteo[2] > humWarn)) {
+    if ((meteo[3] > 2000) || (meteo[2] > 55)) {
       sm.warn();
     }
     screen.flip();
@@ -60,13 +58,14 @@ function main() {
     drawScreen();
   };
 
-  var interSensor = setInterval(function() {
-    dht.read(tempHumCb);
-  }, readWriteDelay);
+  setInterval(newCsv, csv_d * 1000);
 
-  var interSave = setInterval(function() {
+  setInterval(function() {
+    dht.read(tempHumCb);
+  }, rw_d * 1000);
+
+  setInterval(function() {
     fs.appendFileSync(csv, meteo.join(','));
-  }, saveDelay);
+  }, s_d * 1000);
 }
 E.on('init', main);
-main();
